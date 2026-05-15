33e7dcd3054dc92abc71bc51ac47e20a8d4b88a2
TIWI OCTAVIA <tiwioctavia01@gmail.com>
2026-05-15 17:54:19 +0700

    Memperbaiki modal box dan animasi di tampilan mobile

--- script.js
+++ script.js
@@ -1,17 +1,80 @@
-// Toggle classa active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector
 ('.navbar-nav');
 // ketika hamburger menu di klik
 document.querySelector('#hamburger-menu').
-onclick = () => {
+onclick = (e) => {
     navbarNav.classList.toggle('active')
+    e.preventDefault();
 };
 
-// Klik di luar sidebar untuk menghilangkan nav
-const hamburger = document.querySelector('#hamburger-menu');
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form')
+const searchBox = document.querySelector('#search-box')
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  if (searchForm.classList.contains('active')) {
+    searchBox.focus();
+  }
+  
+  e.preventDefault();;
+}
+
+// Toggle kelas actife untuk shopping cart
+const shoppingCart= document.querySelector('.shopping-cart')
+document.querySelector('#shopping-cart-button').onclick = (e) =>{
+  shoppingCart.classList.toggle('active');
+  e.preventDefault();
+}
+
+// Klik di luar selemnt
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
+const sc = document.querySelector('#shopping-cart-button');
 
 document.addEventListener('click', function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
+
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!sc.contains(e.target) && !shoppingCart.contains(e.target)) {
+    shoppingCart.classList.remove('active');
+  }
 });
+
+// Modal Box
+const itemDetailModal = document.querySelector('#item-detail-modal');
+const itemDetailButtons = document.querySelectorAll('.item-detail-button'); // Gunakan selectorAll karena tombolnya banyak
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+itemDetailButtons.forEach((btn) => {
+  btn.onclick = (e) => {
+    itemDetailModal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// Klik tombol close modal
+document.querySelector('.modal .close-icon').onclick = (e) => {
+  itemDetailModal.style.display = 'none';
+  e.preventDefault();
+};
+
+// // klik diluar modal
+// const modal = document.querySelector('#item-detail-modal');
+window.onclick = (e) => {
+  if (e.target === itemDetailModal ){
+    itemDetailModal.style.display= 'none'
+  }
+}
